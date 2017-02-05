7aaccfb842ded9c0ee1f17951a9abb31b2be78db
Christophe Stoll <ch@precious-forever.com>
2017-02-05 20:36:07 +0100

    add icon and description to commands
    to be used in Sketch Runner

--- src/plugin.js
+++ src/plugin.js
@@ -13,7 +13,7 @@ export const HKSketchFusionExtension = {
   description: 'Say goodbye to Lorem Ipsum: populate your Sketch documents with meaningful data.',
   author: 'precious design studio',
   authorEmail: 'info@precious-forever.com',
-  version: '2.0.2',
+  version: '2.1.0',
   identifier: 'com.precious-forever.sketch.datapopulator2',
   compatibleVersion: '3.7',
   menu: {
@@ -31,31 +31,43 @@ export const HKSketchFusionExtension = {
     populateWithPreset: {
       name: 'Populate with Preset',
       shortcut: '',
+      description: 'Pick one of Data Populator\'s built in Presets',
+      icon: '../Resources/populateWithPreset.png',
       run: commands.populateWithPreset
     },
     populateWithJSON: {
       name: 'Populate with JSON',
       shortcut: '',
+      description: 'Pick a local JSON file',
+      icon: '../Resources/populateWithJSON.png',
       run: commands.populateWithJSON
     },
     populateTable: {
       name: 'Populate Table',
       shortcut: '',
+      description: 'Pick CSV file to populate a table',
+      icon: '../Resources/populateTable.png',
       run: commands.populateTable
     },
     populateAgain: {
       name: 'Populate Again',
       shortcut: 'cmd shift x',
+      description: 'Populate again with last used setup',
+      icon: '../Resources/populateAgain.png',
       run: commands.populateAgain
     },
     revealPresets: {
       name: 'Reveal Presets',
       shortcut: '',
+      description: 'Show Data Populator\'s Presets in Finder',
+      icon: '../Resources/revealPresets.png',
       run: commands.revealPresets
     },
     clearLayers: {
       name: 'Clear Layers',
       shortcut: '',
+      description: 'Remove all populated data from selected Layers',
+      icon: '../Resources/clearLayers.png',
       run: commands.clearLayers
     }
   }
