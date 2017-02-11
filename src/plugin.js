d01acc27cb7d5c52d9985fa4bace1752f07a6524
Christophe Stoll <ch@precious-forever.com>
2017-02-11 17:22:25 +0100

    bump version

--- src/plugin.js
+++ src/plugin.js
@@ -13,7 +13,7 @@ export const HKSketchFusionExtension = {
   description: 'Say goodbye to Lorem Ipsum: populate your Sketch documents with meaningful data.',
   author: 'precious design studio',
   authorEmail: 'info@precious-forever.com',
-  version: '2.1.2',
+  version: '2.1.3',
   identifier: 'com.precious-forever.sketch.datapopulator2',
   compatibleVersion: '3.7',
   menu: {
