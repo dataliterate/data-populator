86416391c370a814f6bd6a4d2ee4b17b949ad8aa
Lukas <lukasondrej@icloud.com>
2017-06-19 15:39:28 +0100

    Review compatibility + create initial appcast

--- src/plugin.js
+++ src/plugin.js
@@ -13,9 +13,9 @@ export const HKSketchFusionExtension = {
   description: 'Say goodbye to Lorem Ipsum: populate your Sketch documents with meaningful data.',
   author: 'precious design studio',
   authorEmail: 'info@precious-forever.com',
-  version: '2.1.3',
+  version: '2.1.4',
   identifier: 'com.precious-forever.sketch.datapopulator2',
-  compatibleVersion: '3.7',
+  compatibleVersion: '43',
   menu: {
     'isRoot': false,
     'items': [
