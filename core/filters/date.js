da0ef9e1e43b3e13575d3ac82a00f5966dea89a3
Dennis Rosenfeld <drosenfeld@appneta.com>
2021-04-20 15:37:28 -0700

    cleanup

--- core/filters/date.js
+++ core/filters/date.js
@@ -7,7 +7,7 @@ export const name = 'date'
 export const alias = 'date'
 
 /**
- * Trims the input string to a max number of characters.
+ * Convert timestamps into formatted dates
  *
  * @param {String} string
  * @param {String} param
@@ -16,42 +16,15 @@ export const alias = 'date'
 
 export function apply(string, param) {
 
-//   if (!string) return
+  if (!string) return
 
 // if we have no params, we can default to YYYY-MM-DD
 //if (!param) return string
 
-
-  if (!string) {
-    console.log("no string...")
-    return
-    }
-
-
-  if (!param) {
-        console.log("no params...")
-        // return string
-    }
-
-
-
-
-//TESTING:
-console.log("string:" + string);
-if (typeof string === 'string' || string instanceof String)
-{console.log('its a string');
-}
-if (typeof string === 'number' || string instanceof number)
-{console.log('its a number');
-}
-
-
 //get date formatting rules
 let dateFormat = param ? String(param) : 'YYYY-MM-DD'
+
 return moment(string).format(dateFormat);
-
-
-// return moment.unix(1293683278).format('YYYY-MM-DD');
 }
 
 
